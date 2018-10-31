8c87564dcc219d981886f786832f74d2937c9de2
Jeril George Kadavan <jerilgkadavan@gmail.com>
2018-10-30 23:27:26 -0400

    greensock correction

--- js/main.js
+++ js/main.js
@@ -2,4 +2,24 @@
 
 (() => {
   console.log("mainjs");
+
+// init controller
+// var controller = new ScrollMagic.Controller();
+
+// // loop through all elements
+// $('#graphImg').each(function() {
+  
+//   // build a tween
+//   var tween = TweenMax.from($(this), 0.3, {autoAlpha: 0, scale: 0.5, y: '+=30', ease:Linear.easeNone});
+
+//   // build a scene
+//   var scene = new ScrollMagic.Scene({
+//     triggerElement: this
+//   })
+//   .setTween(tween) // trigger a TweenMax.to tween
+//   .addTo(controller);
+  
+// });
+
+
 })();
