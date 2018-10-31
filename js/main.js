415d6329244395f29d89489aca90dec1e6623c25
Jeril George Kadavan <jerilgkadavan@gmail.com>
2018-10-31 19:06:56 -0400

    js

--- js/main.js
+++ js/main.js
@@ -20,6 +20,39 @@
 //   .addTo(controller);
   
 // });
+var waypoint = new Waypoint({
+  element: document.getElementById('waypoint'),
+  handler: function(direction) {
+    console.log('Scrolled to waypoint!')
+  }
+})
+
+  // console.log("tween max");
+
+var timeline = new TimelineMax();
+
+timeline.from('.firstHeading',2,{y:1000, opacity:.1, ease: Bounce.easeOut});
+timeline.from('#headerImage',2.5,{scale:0, rotation:-500});
+timeline.from('.keyFeatureHeading',2,{x:-1000, opacity:.1, ease: Bounce.easeOut});
+timeline.from('.keyFeatureImg1',.5,{opacity: 0});
+timeline.from('#keyFeatureSvg1',.2,{opacity: 0});
+timeline.from('.keyFeatureImg2',.5,{opacity: 0});
+timeline.from('#keyFeatureSvg2',.2,{opacity: 0});
+timeline.from('.keyFeatureImg3',.5,{opacity: 0});
+timeline.from('#keyFeatureSvg3',.2,{opacity: 0});
+timeline.from('.whyResponsiveHeading',2,{x:1000, opacity:.1, ease: Bounce.easeOut});
+timeline.from('.whyResponsiveImg1',.5,{opacity: 0});
+timeline.from('#whyResponsiveSvg1',.2,{opacity: 0});
+timeline.from('.whyResponsiveImg2',.5,{opacity: 0});
+timeline.from('#whyResponsiveSvg2',.2,{opacity: 0});
+timeline.from('.fourthHeading',2,{y:-1000, opacity:0, ease: Bounce.easeOut});
+timeline.from('#graphImg',1,{opacity:0, scale:.5});
+
+//scrollmagic
+
+
+
+
 
 
 })();
